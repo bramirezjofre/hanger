75aaa947edf4a37f3abfb5ecab4c0d9df29b36c1
Martina Pauer <martinapauer38@gmail.com>
2026-06-01 12:51:34 +0000

    Starting To Implement UML diagrams and strtucture

---
+++ src/base/interviewer.py
@@ -0,0 +1,39 @@
+class loadUser:
+    
+    def __init__(self):
+        '''
+            When The Interviewer proof that
+            the person reach the objetives
+            then write the contact for send
+            registration steps changing the
+            algorithm for kind of contact.
+        '''
+        # Dictonary "Address" To "Kind Of Contact"
+        self.contacts: dict[str, str] = []
+
+    def send_steps(self):
+        '''
+            Filter by kind of contact and sends
+            steps of different way with link to
+            registration flask web app
+        '''
+        for contact in self.contacts.values():
+            
+            contact: str = contact.lower()
+            address: str = self.contacts[contact]
+            
+            if contact.__contains__('mail'):
+                pass
+            elif contact.__contains__('tel'):
+                pass
+            elif contact == 'ig':
+                pass
+            
+            del address
+
+    def add_user(self, kind: str, contact_address: str):
+        '''
+            Add a New Contact to Contacts list for
+            send steps later
+        '''
+        self.contacts.__setitem__(contact_address, kind)
