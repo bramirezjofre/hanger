3764a8e50390c05f57cbd4d6ab1d51430b3badf9
Martina Pauer <martinapauer38@gmail.com>
2026-06-01 13:24:09 +0000

    Prepare Flask Strtucture

--- src/hanger.py
+++ src/hanger.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+from flask import Flask
+from flask import request
+import base.user
+
+web = Flask(__name__)
--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+from flask import Flask
+from flask import request
+import base.interviewer
+
+loads = Flask(__name__)
