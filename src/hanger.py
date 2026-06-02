ee8e57990595a1505fd785e711de5adc9431e502
Martina Pauer <martinapauer38@gmail.com>
2026-06-02 13:52:22 +0000

    End Flask Support Details

--- src/hanger.py
+++ src/hanger.py
@@ -6,6 +6,7 @@ import base.user
 
 web = Flask(__name__)
 log = base.user.Profile()
+users: list[str] = [log, log, log]
 
 @web.route('/', methods = ['GET', 'POST'])
 def main() -> str:
@@ -34,12 +35,77 @@ def logged() -> str:
         log.name = request.form['hanger-user']
         log.age = hex(hash(request.form['hanger-password']))
         log.contact['mail'] = request.form['hanger-user']
-        os.system('rm -R /workspaces/hanger/src/__pyc* && rm -R /workspaces/hanger/base/__pyc*')
-        del os
+        try:
+            os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+        except:
+            pass    
+        
         return  log.get_data().__str__()
 
-#@web.route('/registered', methods = ['POST'])
-#@web.route('/load', methods = ['POST'])
-#@web.route('/interviewer-report', methods = ['POST'])
-#@web.route('/hanger-steps', methods = ['POST'])
-#@web.route('/chatting', methods = ['POST'])
+@web.route('/registered', methods = ['POST'])
+def register_ends() -> str:
+    try:
+        os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+    except:
+        pass
+    
+    return '<h1>Congrat You\'re Hanger New User.</h1>'
+
+@web.route('/load', methods = ['POST'])
+def load_ends() -> str:
+    users.append(f'<li>{request.form['contact']}</li>')
+    try:
+        os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+    except:
+        pass
+
+    return f'<or>{users}</ol>'
+
+
+@web.route('/interviewer-report', methods = ['GET', 'POST'])
+def make_report() -> str:
+    reporting = open('loadUserForm.html', 'r')
+    content: str = ''
+    
+    for line in reporting.readlines():
+        content += line
+
+    reporting.close()
+    del reporting
+    
+    try:
+        os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+    except:
+        pass
+
+    return content
+
+@web.route('/hanger-steps', methods = ['POST'])
+def steps() -> str:
+    content: str = ''
+    
+    for user in users:
+        content += f'Send Page To {user}'
+        
+
+    try:
+        os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+    except:
+        pass
+        
+    return content    
+@web.route('/chatting', methods = ['POST'])
+def chat() -> str:
+    chatter = base.user.HangerMessage()
+    chatter.sender = log
+    chatter.receiver = users[1]
+
+    messages: str = f'<h1>From {chatter.sender.name} To {chatter.receiver.name}</h1>'
+    
+    chatter.chat_messages += request.form['chat']
+    try:
+        os.system('rm -R /workspaces/hanger/src/__pycache__ && rm -R /workspaces/hanger/src/base/__pycache__')
+    except:
+        pass
+    
+    return messages
