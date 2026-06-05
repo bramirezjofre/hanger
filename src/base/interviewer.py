08b50794b58a9b85e8f82f0653bdca051f01bd8c
Martina Pauer <martinapauer38@gmail.com>
2026-06-05 22:15:21 +0000

    Add Send Mail Later Complete Steps sending

--- src/base/interviewer.py
+++ src/base/interviewer.py
@@ -25,7 +25,29 @@ class loadUser:
             address: str = self.contacts[contact]
             
             if contact.__contains__('mail'):
-                pass
+                # RFC 821 Standard
+                import smtplib
+                # Security Layer For Addresses
+                import aiosmtpd
+                import ssl
+                from getpass import getpass
+
+                port: int = 465
+                
+                server: str = 'smtp.gmail.com'
+                hanger_no_reply: str = '<hanger_no-reply@gmail.com>'
+                password: str = getpass().encode('utf-32')
+                
+                ssl_layer = ssl.create_default_context()
+                
+                with smtplib.SMTP_SSL(server, port, context = ssl_layer) as back:
+                    # Only expose password when is needed and delete for externs 
+                    back.login(hanger_no_reply, password.decode('utf-32'))
+                    del password
+                    # Send Mail With Next Steps
+                    back.sendmail(hanger_no_reply, self.contacts[contact])
+
+                
             elif contact.__contains__('tel'):
                 pass
             elif contact == 'ig':
