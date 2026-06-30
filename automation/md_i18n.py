17dc13f20155dadfb9025e1d0272cee8e49f75a6
Martina Pauer <martinapauer38@gmail.com>
2026-06-30 14:46:32 +0000

    Add Folder Iteration For Get All md Files

--- automation/md_i18n.py
+++ automation/md_i18n.py
@@ -48,8 +48,49 @@ if __name__ == '__main__':
 
     agent = Documents(f'{root_folder}/AGENTS.md', 1, 40)
 
-    md_list =   [technical, roadmap, readme, agent]
-    # Get files in sub-folders
+    md_list: list[str] =   [technical, roadmap, readme, agent]
+    # Get files in sub-folders (Only md)
+    dirs: list[str] = os.listdir(f'{root_folder}')
+
+    def get_files(folder: str):
+        '''
+            Auxiliar Function For Could
+            use recursivity to iterates
+            over all sub folders
+        '''
+        # Get Files From Root (Level 1) to Last Folder With md files (Level 5)
+        for file_name in dirs:
+            # Level 1
+            if os.path.isdir(file_name):
+                # Iterative Case: is Directory
+                dirs_1 = os.listdir(file_name)
+                if (not dirs.__contains__(dirs_1[0])):
+                    dirs.__add__(dirs_1)
+                for names_1 in dirs_1:
+                    # Level 2
+                    dirs_2 = os.listdir(names_1)
+                    if (not dirs.__contains__(dirs_2[0])):
+                        dirs.__add__(dirs_2)
+                    for names_2 in dirs_2:
+                        # Level 3
+                        dirs_3 = os.listdir(names_2)
+                        if (not dirs.__contains__(dirs_3[0])):
+                            dirs.__add__(dirs_3)
+                        for names_3 in dirs_3:
+                            # Level 4
+                            dirs_4 = os.listdir(names_3)
+                            if (not dirs.__contains__(dirs_4[0])):
+                                dirs.__add__(dirs_4)
+                            for names_4 in dirs_4:
+                                # Level 5
+                                dirs_5 = os.listdir(names_4)
+                                if (not dirs.__contains__(dirs_5[0])):
+                                    dirs.__add__(dirs_5)    
+                        
+            elif (file_name.__contains__('.md') and (not md_list.__contains__(file_name))):
+                # Only Add New Files: Base Case, is a file
+                md_list.append(f'{folder}/{file_name}')
+             
     # Add Translation for each content in respective file
     for language in lang_codes:
         for content in md_list:
